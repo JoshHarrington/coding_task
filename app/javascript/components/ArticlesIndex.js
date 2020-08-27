e633106ce6c065aa80a92913b47fec2e119d72db
Josh Harrington <josh@joshharrington.co.uk>
2020-08-27 17:31:48 +0100

    Add tailwind for css utility classes Set up sorting of article order based on likes Add basic styling of article list with images added

--- app/javascript/components/ArticlesIndex.js
+++ app/javascript/components/ArticlesIndex.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react'
+import { FaArrowAltCircleUp } from 'react-icons/fa'
+import classNames from 'classnames'
 
 function combineDbClientArticles({dbArticles, clientArticles}) {
 	const updatedArticles = clientArticles.map(a => {
@@ -9,7 +11,8 @@ function combineDbClientArticles({dbArticles, clientArticles}) {
 		}
 		return {...a, likes: a.reactions.likes + databaseArticleLikeNum}
 	})
-	return updatedArticles
+	const updatedArticlesSorted = updatedArticles.sort((a,b) => b.likes - a.likes)
+	return updatedArticlesSorted
 }
 
 function fetchArticleData({updateArticlesList, databaseArticles}) {
@@ -42,22 +45,41 @@ function addArticleLike({id, csrfToken, articles, updateArticlesList}) {
 
 const ArticlesIndex = ({databaseArticles, csrfToken}) => {
 	const [articlesList, updateArticlesList] = useState([])
+	const [hoveredArticle, updateHoveredArticle] = useState(null)
 
 	useEffect(() => {
 		fetchArticleData({updateArticlesList, databaseArticles})
 	}, [])
 
 	return (
-		<main>
-			<h1>Articles List</h1>
-			<ul>
-				{articlesList.map(a => (
-					<li
-						key={a.id}
-						onClick={() => addArticleLike({id: a.id, csrfToken, articles: articlesList, updateArticlesList})}
-					>{a.title} likes: {a.likes}</li>
-				))}
-			</ul>
+		<main className="p-4 flex justify-center" >
+			<div className="max-w-full" style={{width:1000}}>
+
+				<h1 className="text-3xl mb-2">Articles List</h1>
+				<ul>
+					{articlesList.map(a => (
+						<li
+							className="mb-2 cursor-pointer flex items-center p-2 border rounded border-solid border-gray-200 hover:border-gray-400"
+							key={a.id}
+							onClick={() => addArticleLike({id: a.id, csrfToken, articles: articlesList, updateArticlesList})}
+							onMouseOver={() => updateHoveredArticle(a.id)}
+							onMouseLeave={() => updateHoveredArticle(null)}
+						>
+							<div className="flex flex-col mr-3 items-center">
+								<FaArrowAltCircleUp className={classNames("w-6 h-6", {"text-green-600": hoveredArticle === a.id, "text-green-300": hoveredArticle !== a.id})}/>
+								<p className={classNames("text-sm", {"text-gray-900": hoveredArticle === a.id, "text-gray-400": hoveredArticle !== a.id})}>{a.likes}</p>
+							</div>
+							<div className="flex justify-between items-center">
+								<img src={a.images[0].files.small} alt={a.title} className="mr-3" />
+								<div>
+									<h2 className="text-xl">{a.title}</h2>
+									<p>{a.description}</p>
+								</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
 		</main>
 	)
 }
